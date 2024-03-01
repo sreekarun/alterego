9b4757063d58651af8e4099a0f945f87a51b7746
Sree Karunakaran <sreekarun@gmail.com>
2024-02-29 22:27:52 -0500

    First audio integration

---
+++ main.py
@@ -0,0 +1,16 @@
+from openai import OpenAI
+from pydub import AudioSegment
+from pydub.playback import play
+from playsound import playsound
+
+client = OpenAI()
+
+response = client.audio.speech.create(
+    model="tts-1",
+    voice="shimmer",
+    input="Hai Jisna, You are awesome! Let's create something amazing together.",
+)
+response.write_to_file("output.mp3")
+
+# Play the audio
+playsound("output.mp3")
